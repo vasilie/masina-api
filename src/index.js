576418e1afb10d7984ddee712a08b047cb3d3364
_vasilie <vasilije.milenkovic@diwanee.com>
2018-02-06 16:43:03 +0100

    user logged in

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+import auth from './routes/auth';
 
 const app = express();
+mongoose.connect("mongodb://localhost/masina");
 
-app.post('/api/auth', (req, res) => {
-  res.status(400).json({errors:{global:"Invalid credentials"} });
-})
+app.use(bodyParser.json());
+app.use('/api/auth', auth);
 
 app.get('/*', (req, res) =>{
   res.sendFile(path.join(__dirname, 'index.html'));
